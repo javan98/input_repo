237bd675faa9bafb788cf23380c8a0d354c8bd0b
javan <154074254+javan98@users.noreply.github.com>
2024-02-23 01:11:10 +0300

    Delete input_repo directory

--- input_repo/user_input.py
+++
@@ -1,5 +0,0 @@
-import sys
-name = input("What is your name? ")
-age = input("What is your age? ")
-location = input("Where do you live? ")
-print("Hello, " + name + ". You are " + str(age) + " years old and live in " + location)
