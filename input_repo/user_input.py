83dd0f19243b59f1773a46a495df0c83b11ad157
javan <154074254+javan98@users.noreply.github.com>
2024-02-23 00:40:17 +0300

    Add files via upload

---
+++ input_repo/user_input.py
@@ -0,0 +1,5 @@
+import sys
+name = input("What is your name? ")
+age = input("What is your age? ")
+location = input("Where do you live? ")
+print("Hello, " + name + ". You are " + str(age) + " years old and live in " + location)
